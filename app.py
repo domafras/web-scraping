2a777cf9588c29504824927e1b592f1a93fff333
Leonardo Mafra <domafras@gmail.com>
2024-11-24 15:35:22 -0300

    app 01

---
+++ app.py
@@ -0,0 +1,10 @@
+import requests
+
+def fetch_page(url):
+    response = requests.get(url)
+    return response.text
+
+if __name__ == "__main__":
+    url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-titnio-preto-128-gb-8-gb/p/MLB1040287850?product_trigger_id=MLB1040287849&pdp_filters=item_id%3AMLB3846002353&applied_product_filters=MLB1040287849&quantity=1"
+    page_content = fetch_page(url)
+    print(page_content)
